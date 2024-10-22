6357dcfbcd13a52468ac3f6eac498cf0d27a5241
Michael Boyles <michael@boyl.es>
2024-10-22 12:39:40 +0100

    refactor: move getUrl function
    Doesn't need its own file

--- src/lib/use-websocket.ts
+++ src/lib/use-websocket.ts
@@ -1,8 +1,7 @@
 import { useEffect, useRef, useState, useCallback } from 'react';
 import { flushSync } from 'react-dom';
-import { ReadyState } from './constants';
+import { DEFAULT_RECONNECT_INTERVAL_MS, DEFAULT_RECONNECT_LIMIT, ReadyState } from './constants';
 import { createOrJoinSocket } from './create-or-join';
-import { getUrl } from './get-url';
 import {
     Options,
     ReadyStateState,
@@ -122,4 +121,41 @@ export function useWebSocket(
         sendMessage,
         readyState: readyStateFromUrl
     };
+}
+
+async function getUrl(
+    url: string | (() => (string | Promise<string>)),
+    options: Readonly<Options>,
+    retriedAttempts: number = 0,
+): Promise<string | null> {
+    if (typeof url === 'function') {
+        try {
+            return await url();
+        }
+        catch (e) {
+            if (options.retryOnError) {
+                const reconnectLimit = options.reconnectAttempts ?? DEFAULT_RECONNECT_LIMIT;
+                if (retriedAttempts < reconnectLimit) {
+                    const nextReconnectInterval = typeof options.reconnectInterval === 'function' ?
+                        options.reconnectInterval(retriedAttempts) :
+                        options.reconnectInterval;
+
+                    await waitFor(nextReconnectInterval ?? DEFAULT_RECONNECT_INTERVAL_MS);
+                    return getUrl(url, options, retriedAttempts + 1);
+                }
+                else {
+                    options.onReconnectStop?.(retriedAttempts);
+                    return null;
+                }
+            }
+            else {
+                return null;
+            }
+        }
+    }
+    return url;
+}
+
+function waitFor(duration: number) {
+    return new Promise(resolve => window.setTimeout(resolve, duration));
 }
