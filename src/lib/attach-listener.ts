817ccb5a3b321a792997f36b5075a730f00637fb
Michael Boyles <michael@boyl.es>
2025-05-05 19:03:46 +0100

    refactor: inline websocket handlers

--- src/lib/attach-listener.ts
+++ src/lib/attach-listener.ts
@@ -7,48 +7,35 @@ import {
 } from './constants';
 import { Options } from './types';
 
-const bindMessageHandler = (
-  webSocketInstance: WebSocket,
-  optionsRef: MutableRefObject<Options>,
-) => {
-  let heartbeatCb: () => void;
+export function attachListeners(
+    webSocketInstance: WebSocket,
+    setReadyState: (readyState: ReadyState) => void,
+    optionsRef: MutableRefObject<Options>,
+    reconnect: () => void,
+    reconnectCount: MutableRefObject<number>,
+): () => void {
+  let markMessageReceived: () => void;
 
   if (optionsRef.current.heartbeat && webSocketInstance instanceof WebSocket) {
     const heartbeatOptions =
-      typeof optionsRef.current.heartbeat === "boolean"
-        ? undefined
-        : optionsRef.current.heartbeat;
-    heartbeatCb = heartbeat(webSocketInstance, heartbeatOptions);
+        typeof optionsRef.current.heartbeat === "boolean"
+            ? undefined
+            : optionsRef.current.heartbeat;
+    markMessageReceived = heartbeat(webSocketInstance, heartbeatOptions);
   }
 
   webSocketInstance.onmessage = (message: WebSocketEventMap['message']) => {
-    heartbeatCb?.();
+    markMessageReceived?.();
     optionsRef.current.onMessage && optionsRef.current.onMessage(message);
   };
-};
 
-const bindOpenHandler = (
-  webSocketInstance: WebSocket,
-  optionsRef: MutableRefObject<Options>,
-  setReadyState: (readyState: ReadyState) => void,
-  reconnectCount: MutableRefObject<number>,
-) => {
   webSocketInstance.onopen = (event: WebSocketEventMap['open']) => {
     optionsRef.current.onOpen && optionsRef.current.onOpen(event);
     reconnectCount.current = 0;
     setReadyState(ReadyState.OPEN);
   };
-};
 
-const bindCloseHandler = (
-  webSocketInstance: WebSocket,
-  optionsRef: MutableRefObject<Options>,
-  setReadyState: (readyState: ReadyState) => void,
-  reconnect: () => void,
-  reconnectCount: MutableRefObject<number>,
-) => {
   let reconnectTimeout: number | undefined;
-
   webSocketInstance.onclose = (event: WebSocketEventMap['close']) => {
     optionsRef.current.onClose && optionsRef.current.onClose(event);
     setReadyState(ReadyState.CLOSED);
@@ -57,27 +44,22 @@ const bindCloseHandler = (
     }
   };
 
-  return () => reconnectTimeout && window.clearTimeout(reconnectTimeout);
-};
-
-const bindErrorHandler = (
-  webSocketInstance: WebSocket,
-  optionsRef: MutableRefObject<Options>,
-  reconnect: () => void,
-  reconnectCount: MutableRefObject<number>,
-) => {
-  let reconnectTimeout: number | undefined;
-
+  let reconnectTimeout2: number | undefined;
   webSocketInstance.onerror = (error: WebSocketEventMap['error']) => {
     optionsRef.current.onError && optionsRef.current.onError(error);
 
     if (optionsRef.current.retryOnError) {
-      reconnectTimeout = reconnectIfBelowAttemptLimit(optionsRef, reconnectCount, reconnect);
+      reconnectTimeout2 = reconnectIfBelowAttemptLimit(optionsRef, reconnectCount, reconnect);
     }
   };
 
-  return () => reconnectTimeout && window.clearTimeout(reconnectTimeout);
-};
+  return () => {
+    setReadyState(ReadyState.CLOSING);
+    reconnectTimeout && window.clearTimeout(reconnectTimeout);
+    reconnectTimeout2 && window.clearTimeout(reconnectTimeout2);
+    webSocketInstance.close();
+  };
+}
 
 function reconnectIfBelowAttemptLimit(
     optionsRef: MutableRefObject<Options>,
@@ -101,49 +83,3 @@ function reconnectIfBelowAttemptLimit(
     console.warn(`Max reconnect attempts of ${reconnectAttempts} exceeded`);
   }
 }
-
-export const attachListeners = (
-    webSocketInstance: WebSocket,
-    setReadyState: (readyState: ReadyState) => void,
-    optionsRef: MutableRefObject<Options>,
-    reconnect: () => void,
-    reconnectCount: MutableRefObject<number>,
-  ): (() => void) => {
-
-  let cancelReconnectOnClose: () => void;
-  let cancelReconnectOnError: () => void;
-
-  bindMessageHandler(
-    webSocketInstance,
-    optionsRef,
-  );
-
-  bindOpenHandler(
-    webSocketInstance,
-    optionsRef,
-    setReadyState,
-    reconnectCount,
-  );
-
-  cancelReconnectOnClose = bindCloseHandler(
-    webSocketInstance,
-    optionsRef,
-    setReadyState,
-    reconnect,
-    reconnectCount,
-  );
-
-  cancelReconnectOnError = bindErrorHandler(
-    webSocketInstance,
-    optionsRef,
-    reconnect,
-    reconnectCount,
-  );
-
-  return () => {
-    setReadyState(ReadyState.CLOSING);
-    cancelReconnectOnClose();
-    cancelReconnectOnError();
-    webSocketInstance.close();
-  };
-};
