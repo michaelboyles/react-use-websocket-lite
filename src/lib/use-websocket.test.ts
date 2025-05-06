978a0510999912d41677a7eb973f5861e15bc914
Michael Boyles <michael@boyl.es>
2025-05-06 12:18:22 +0100

    test: make tests much faster

--- src/lib/use-websocket.test.ts
+++ src/lib/use-websocket.test.ts
@@ -2,20 +2,16 @@ import { afterEach, beforeEach, expect, test, vi } from "vitest";
 import { renderHook, waitFor } from '@testing-library/react';
 import { useWebSocket } from './use-websocket';
 import WS from "jest-websocket-mock";
-import { Options } from './types';
 import { ReadyState } from './constants';
 
 let server: WS;
 const URL = 'ws://localhost:1234';
 const noop = () => { };
-const DEFAULT_OPTIONS: Options = { url: URL };
-let options: Options;
 const sleep = (duration: number): Promise<void> => new Promise(resolve => setTimeout(() => resolve(), duration));
 console.error = noop;
 
 beforeEach(() => {
     server = new WS(URL);
-    options = { ...DEFAULT_OPTIONS };
 });
 
 afterEach(() => {
@@ -23,10 +19,10 @@ afterEach(() => {
 });
 
 test('readyState changes across readyState transitions', async () => {
-    const {
-        result,
-        rerender,
-    } = renderHook(({ connect }) => useWebSocket({ ...options, connect }), {
+    const { result, rerender } = renderHook(({ connect }) => useWebSocket({
+        url: URL,
+        connect
+    }), {
         initialProps: { connect: false }
     })
 
@@ -42,16 +38,15 @@ test('readyState changes across readyState transitions', async () => {
 })
 
 test('a function-promise based url works the same as a string-based url', async () => {
-    const getUrl = () => {
-        return new Promise<string>(resolve => {
-            setTimeout(() => resolve(URL), 1000);
-        });
+    async function getUrl() {
+        await sleep(100);
+        return URL;
     }
 
-    const {
-        result,
-        rerender,
-    } = renderHook(({ connect }) => useWebSocket({ ...options, url: getUrl, connect }), {
+    const { result, rerender } = renderHook(({ connect }) => useWebSocket({
+        url: getUrl,
+        connect
+    }), {
         initialProps: { connect: false }
     })
 
@@ -63,190 +58,162 @@ test('a function-promise based url works the same as a string-based url', async 
     expect(result.current.readyState).toEqual(ReadyState.OPEN);
 
     server.close();
-    await expect(result.current.readyState).toEqual(ReadyState.CLOSED);
+    expect(result.current.readyState).toEqual(ReadyState.CLOSED);
 })
 
 test('a function-promise based url retries until it resolves if retryOnError is true', async () => {
     let attemptsUntilSuccess = 2;
-    options.retryOnError = true;
-    options.maxReconnectAttempts = 3;
-    options.reconnectInterval = 500;
-    const onReconnectStopFn1 = vi.fn();
-    options.onReconnectStop = onReconnectStopFn1;
-
     const getUrl = () => {
         return new Promise<string>((resolve, reject) => {
             if (attemptsUntilSuccess > 0) {
                 attemptsUntilSuccess--;
                 reject('Failed to get url');
-            } else {
+            }
+            else {
                 resolve(URL);
             }
         });
     };
 
-    const {
-        result,
-        rerender,
-    } = renderHook(({ connect }) => useWebSocket({...options, url: getUrl, connect }), {
+    const onReconnectStop = vi.fn();
+    const { result, rerender } = renderHook(({ connect }) => useWebSocket({
+        url: getUrl,
+        retryOnError: true,
+        maxReconnectAttempts: 3,
+        reconnectInterval: 20,
+        onReconnectStop,
+        connect
+    }), {
         initialProps: { connect: false }
     });
 
     expect(result.current.readyState).toEqual(ReadyState.UNINSTANTIATED);
     rerender({ connect: true });
-    await sleep(1000);
     expect(result.current.readyState).toEqual(ReadyState.CONNECTING);
-    await sleep(1000);
-    await expect(result.current.readyState).toEqual(ReadyState.OPEN);
-    expect(options.onReconnectStop).not.toHaveBeenCalled();
+    await expect.poll(
+        () => result.current.readyState,
+        { interval: 5, timeout: 1_000 }
+    ).toEqual(ReadyState.OPEN);
+    expect(onReconnectStop).not.toHaveBeenCalled();
 });
 
 test('a function-promise based url stops retrying if it has exceeded reconnectAttempts', async () => {
     let attemptsUntilSuccess = 3;
-    options.retryOnError = true;
-    options.maxReconnectAttempts = 2;
-    options.reconnectInterval = 500;
-    const onReconnectStopFn1 = vi.fn();
-    options.onReconnectStop = onReconnectStopFn1;
-
     const getUrl = () => {
         return new Promise<string>((resolve, reject) => {
             if (attemptsUntilSuccess > 0) {
                 attemptsUntilSuccess--;
                 reject('Failed to get url');
-            } else {
+            }
+            else {
                 resolve(URL);
             }
         });
     };
 
-    const {
-        result,
-        rerender,
-    } = renderHook(({ connect }) => useWebSocket({...options, url: getUrl, connect }), {
+    const onReconnectStop = vi.fn();
+    const { result, rerender } = renderHook(({ connect }) => useWebSocket({
+        url: getUrl,
+        retryOnError: true,
+        maxReconnectAttempts: 2,
+        reconnectInterval: 20,
+        onReconnectStop,
+        connect
+    }), {
         initialProps: { connect: false }
     });
 
     expect(result.current.readyState).toEqual(ReadyState.UNINSTANTIATED);
     rerender({ connect: true });
-    await sleep(1000);
     expect(result.current.readyState).toEqual(ReadyState.CONNECTING);
-    await sleep(1000);
-    await expect(result.current.readyState).toEqual(ReadyState.CLOSED);
-    expect(options.onReconnectStop).toHaveBeenCalled();
+    await sleep(100);
+    expect(result.current.readyState).toEqual(ReadyState.CLOSED);
+    expect(onReconnectStop).toHaveBeenCalled();
 });
 
 test('a function-promise based url does not retry if retryOnError is false', async () => {
-    let attemptsUntilSuccess = 2;
-    options.retryOnError = false;
-    options.maxReconnectAttempts = 3;
-    options.reconnectInterval = 500;
-    const onReconnectStopFn1 = vi.fn();
-    options.onReconnectStop = onReconnectStopFn1;
+    const onReconnectStop = vi.fn();
 
-    const getUrl = () => {
-        return new Promise<string>((resolve, reject) => {
-            if (attemptsUntilSuccess > 0) {
-                attemptsUntilSuccess--;
-                reject('Failed to get url');
-            } else {
-                resolve(URL);
-            }
-        });
-    };
+    const getUrl = () => Promise.reject("Failed to get URL");
 
-    const {
-        result,
-        rerender,
-    } = renderHook(({ connect }) => useWebSocket({...options, url: getUrl, connect }), {
+    const { result, rerender } = renderHook(({ connect }) => useWebSocket({
+        url: getUrl,
+        retryOnError: false,
+        onReconnectStop,
+        connect
+    }), {
         initialProps: { connect: false }
     });
 
     expect(result.current.readyState).toEqual(ReadyState.UNINSTANTIATED);
     rerender({ connect: true });
-    await sleep(1000);
-    expect(result.current.readyState).toEqual(ReadyState.CLOSED);
-    await sleep(1000);
-    expect(options.onReconnectStop).not.toHaveBeenCalled();
+    await expect.poll(
+        () => result.current.readyState,
+        { interval: 5, timeout: 500 }
+    ).toEqual(ReadyState.CLOSED);
+    expect(onReconnectStop).not.toHaveBeenCalled();
 });
 
-test('sendMessage passes message to websocket and sends to server', async () => {
-    const {
-        result,
-    } = renderHook(() => useWebSocket(options))
+test('sendMessage sends a message to the server', async () => {
+    const { result: websocket } = renderHook(() => useWebSocket({ url: URL }))
     await server.connected;
-    result.current.sendMessage("Hello");
+    websocket.current.sendMessage("Hello");
     await expect(server).toReceiveMessage("Hello");
 })
 
 test('if sendMessage is called before the websocket opens, the message will be queued and sent when the websocket opens', async () => {
-    const {
-        result,
-    } = renderHook(() => useWebSocket(options))
-    expect(result.current.readyState).not.toEqual(ReadyState.OPEN);
-    result.current.sendMessage("Hello");
+    const { result: websocket } = renderHook(() => useWebSocket({ url: URL }))
+    expect(websocket.current.readyState).not.toEqual(ReadyState.OPEN);
+    websocket.current.sendMessage("Hello");
     await expect(server).toReceiveMessage("Hello");
 })
 
 test('getWebSocket returns the underlying websocket', async () => {
-    const {
-        result
-    } = renderHook(() => useWebSocket(options))
+    const { result } = renderHook(() => useWebSocket({ url: URL }))
     await server.connected;
     const ws = result.current.getWebSocket();
-
-    expect(ws instanceof WebSocket).toBe(true);
+    expect(ws).toBeInstanceOf(WebSocket);
 
     ws?.close();
     await waitFor(() => expect(result.current.readyState).toBe(ReadyState.CLOSED));
 })
 
 test('websocket is closed when the component unmounts', async () => {
-    const {
-        result,
-        unmount,
-    } = renderHook(() => useWebSocket(options))
+    const { result, unmount } = renderHook(() => useWebSocket({ url: URL }))
     await server.connected;
     const ws = result.current.getWebSocket();
 
     unmount();
     expect(ws?.readyState).toBe(ReadyState.CLOSING);
-    await sleep(500);
-    expect(ws?.readyState).toBe(ReadyState.CLOSED);
+    await expect.poll(
+        () => ws?.readyState,
+        { interval: 5, timeout: 500 }
+    ).toBe(ReadyState.CLOSED);
 })
 
 test('Websocket can reconnect after timeout', async () => {
-    options.messageTimeout = 100;
-    options.reconnectInterval = 10;
-    options.maxReconnectAttempts = 10;
-    options.shouldReconnect = () => true;
-
-    const {
-        result: component1,
-    } = renderHook(() => useWebSocket(options))
+    const { result } = renderHook(() => useWebSocket({
+        url: URL,
+        messageTimeout: 100,
+        reconnectInterval: 10,
+        shouldReconnect: () => true
+    }))
 
     await server.connected;
-    expect(component1.current.getWebSocket()?.readyState).toBe(WebSocket.OPEN);
+    expect(result.current.getWebSocket()?.readyState).toBe(WebSocket.OPEN);
 
     await sleep(200);
 
     await server.connected;
-    server.send('pong');
-    expect(component1.current.getWebSocket()?.readyState).toBe(WebSocket.OPEN);
+    expect(result.current.getWebSocket()?.readyState).toBe(WebSocket.OPEN);
 });
 
-test('Options#protocols pass the value on to the instantiated WebSocket', async () => {
-    options.protocols = 'chat';
-
-    const {
-        result
-    } = renderHook(() => useWebSocket(options));
+test('Options#protocols passes the value on to the instantiated WebSocket', async () => {
+    const { result } = renderHook(() => useWebSocket({ url: URL, protocols: "chat" }));
 
     await waitFor(() => {
         const ws = result.current.getWebSocket();
-        if (ws instanceof WebSocket) {
-            expect(ws?.protocol).toEqual('chat');
-        }
+        expect(ws?.protocol).toEqual("chat");
     });
 });
 
@@ -254,20 +221,19 @@ test('Multiple websockets will be opened for the same url', async () => {
     const onConnectionFn = vi.fn();
     server.on('connection', onConnectionFn);
 
-    renderHook(() => useWebSocket(options));
-    renderHook(() => useWebSocket(options));
-    renderHook(() => useWebSocket(options));
+    renderHook(() => useWebSocket({ url: URL }));
+    renderHook(() => useWebSocket({ url: URL }));
+    renderHook(() => useWebSocket({ url: URL }));
 
-    await sleep(500);
-
-    expect(onConnectionFn).toHaveBeenCalledTimes(3);
+    await expect.poll(
+        () => onConnectionFn,
+        { interval: 5, timeout: 500 }
+    ).toHaveBeenCalledTimes(3);
 });
 
 test('Options#onOpen is called with the open event when the websocket connection opens', async () => {
     const onOpenFn = vi.fn();
-    options.onOpen = onOpenFn;
-
-    renderHook(() => useWebSocket(options));
+    renderHook(() => useWebSocket({ url: URL, onOpen: onOpenFn }));
     await server.connected;
     expect(onOpenFn).toHaveBeenCalledTimes(1);
     expect(onOpenFn.mock.calls[0][0].constructor.name).toBe('Event');
@@ -275,9 +241,7 @@ test('Options#onOpen is called with the open event when the websocket connection
 
 test('Options#onClose is called with the close event when the websocket connection closes', async () => {
     const onCloseFn = vi.fn();
-    options.onClose = onCloseFn;
-
-    renderHook(() => useWebSocket(options));
+    renderHook(() => useWebSocket({ url: URL, onClose: onCloseFn }));
     await server.connected;
 
     server.close();
@@ -289,9 +253,7 @@ test('Options#onClose is called with the close event when the websocket connecti
 
 test('Options#onMessage is called with the MessageEvent when the websocket receives a message', async () => {
     const onMessageFn = vi.fn();
-    options.onMessage = onMessageFn;
-
-    renderHook(() => useWebSocket(options));
+    renderHook(() => useWebSocket({ url: URL, onMessage: onMessageFn }));
     await server.connected;
 
     server.send('Hello');
@@ -304,9 +266,7 @@ test('Options#onMessage is called with the MessageEvent when the websocket recei
 
 test('Options#onError is called when the websocket connection errors out', async () => {
     const onErrorFn = vi.fn();
-    options.onError = onErrorFn;
-
-    renderHook(() => useWebSocket(options));
+    renderHook(() => useWebSocket({ url: URL, onError: onErrorFn }));
     await server.connected;
 
     server.error();
@@ -317,103 +277,91 @@ test('Options#onError is called when the websocket connection errors out', async
     });
 });
 
-test('Options#shouldReconnect controls whether a closed websocket should attempt to reconnect', async () => {
-    options.shouldReconnect = () => false;
-    options.reconnectInterval = 500; //Default interval is too long for tests
-
+test('Options#shouldReconnect = false will not reconnect after server disconnection', async () => {
     const onConnectionFn = vi.fn((ws: any) => ws.close());
     server.on('connection', onConnectionFn);
 
-    renderHook(() => useWebSocket(options));
-    await sleep(600);//100ms buffer to avoid race condition
-
+    renderHook(() => useWebSocket({
+        url: URL,
+        reconnectInterval: 20,
+        shouldReconnect: () => false
+    }));
+    await sleep(100);
     expect(onConnectionFn).toHaveBeenCalledTimes(1);
-
-    options.shouldReconnect = () => true;
-
-    renderHook(() => useWebSocket(options));
-    await sleep(600);
-    expect(onConnectionFn).toHaveBeenCalledTimes(3);
 });
 
-test('Options#onReconnectStop is called when the websocket exceeds maximum reconnect attempts provided in options, or 20 by default', async () => {
-    options.shouldReconnect = () => true;
-    options.maxReconnectAttempts = 3;
-    options.reconnectInterval = 500; //Default interval is too long for tests
-    const onReconnectStopFn = vi.fn((numAttempts: number) => { });
-    options.onReconnectStop = onReconnectStopFn;
-
-    renderHook(() => useWebSocket(options));
+test('Options#onReconnectStop is called when the websocket exceeds maximum reconnect attempts', async () => {
+    const onReconnectStopFn = vi.fn((_numAttempts: number) => {});
+    renderHook(() => useWebSocket({
+        url: URL,
+        maxReconnectAttempts: 3,
+        reconnectInterval: 50,
+        onReconnectStop: onReconnectStopFn,
+        shouldReconnect: () => true
+    }));
     await server.connected;
     server.close();
     expect(onReconnectStopFn).not.toHaveBeenCalled();
 
-    await sleep(2000);
-
-    expect(onReconnectStopFn).toHaveBeenCalled();
+    await expect.poll(() => onReconnectStopFn, { interval: 10, timeout: 1_000 }).toHaveBeenCalled()
     expect(onReconnectStopFn.mock.calls[0][0]).toBe(3);
 });
 
-test('Options#retryOnError controls whether a websocket should attempt to reconnect after an error event', async () => {
-    options.retryOnError = false;
-    options.maxReconnectAttempts = 3;
-    options.reconnectInterval = 500;
-    const onReconnectStopFn1 = vi.fn();
-    options.onReconnectStop = onReconnectStopFn1;
-
-    renderHook(() => useWebSocket(options));
+test('Options#retryOnError = false will not reconnect after an error event', async () => {
+    const onReconnectStopFn = vi.fn();
+    renderHook(() => useWebSocket({
+        url: URL,
+        retryOnError: false,
+        reconnectInterval: 50,
+        onReconnectStop: onReconnectStopFn
+    }));
     await server.connected;
-
     server.error();
-    await sleep(1600);
-    expect(onReconnectStopFn1).not.toHaveBeenCalled();
+    await sleep(150);
+    expect(onReconnectStopFn).not.toHaveBeenCalled();
+});
 
-    options.retryOnError = true;
-    const onReconnectStopFn2 = vi.fn();
-    options.onReconnectStop = onReconnectStopFn2;
-
-    renderHook(() => useWebSocket(options));
+test('Options#retryOnError = true will reconnect after an error event', async () => {
+    const onReconnectStop = vi.fn();
+    renderHook(() => useWebSocket({
+        url: URL,
+        retryOnError: true,
+        reconnectInterval: 50,
+        maxReconnectAttempts: 3,
+        onReconnectStop
+    }));
     await server.connected;
-
-    server.error();
-    await sleep(1600);
-    expect(onReconnectStopFn2).toHaveBeenCalled();
+    server.error(); // also closes the server
+    await expect.poll(() => onReconnectStop, { interval: 5, timeout: 500 }).toHaveBeenCalled();
 });
 
 test('Options#heartbeat, if provided, sends a message to the server at the specified interval', async () => {
-    options.heartbeat = {
-        message: 'ping',
-        interval: 500,
-    };
-    renderHook(() => useWebSocket(options));
-
+    let ping = 1;
+    renderHook(() => useWebSocket({
+        url: URL,
+        heartbeat: {
+            message: () => "ping" + (ping++),
+            interval: 10
+        }
+    }));
     await server.connected;
-    await sleep(1600);
-    await expect(server).toHaveReceivedMessages(["ping", "ping", "ping"]);
+    await expect.poll(() => server, { interval: 5, timeout: 100 })
+        .toHaveReceivedMessages(["ping1", "ping2", "ping3"]);
 });
 
 test('Options#messageTimeout, if provided, close websocket if no message is received from server within specified timeout', async () => {
-    options.messageTimeout = 1000;
-    const { result } = renderHook(() => useWebSocket(options));
-
+    const { result } = renderHook(() => useWebSocket({ url: URL, messageTimeout: 25 }));
     await server.connected;
-    await sleep(1600);
-    expect(result.current.readyState).toBe(WebSocket.CLOSED);
+    await expect.poll(() => result.current.readyState, { interval: 5, timeout: 300 })
+        .toBe(WebSocket.CLOSED);
 });
 
 test('Options#messageTimeout, if provided, do not close websocket if a message is received from server within specified timeout', async () => {
-    options.messageTimeout = 1000;
-
-    const { result } = renderHook(() => useWebSocket(options));
-
+    const { result } = renderHook(() => useWebSocket({ url: URL, messageTimeout: 25 }));
     await server.connected;
-    server.send('ping')
-    await sleep(500);
-    server.send('ping')
-    await sleep(500);
-    server.send('ping')
-    await sleep(500);
-    server.send('ping')
-    await sleep(500);
+    for (let i = 0; i < 4; i++) {
+        server.send('ping')
+        await sleep(10);
+    }
     expect(result.current.readyState).toBe(WebSocket.OPEN);
 });
